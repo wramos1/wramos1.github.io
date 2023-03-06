b4dff4e8cce3a900d6128eb82805e33536f08bb1
Willyam Ramos <ramoswillyam1@gmail.com>
2023-03-06 00:12:05 -0800

    Made data structures containing project info in order to map over them

---
+++ src/pages/Work.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Work = () => {
+    return (
+        <div id='myWork'>
+            <h1 className="section-title">
+                My Work
+            </h1>
+            <div className='highlighted-projects'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Work
---
+++ src/utils/Projects.js
@@ -0,0 +1,67 @@
+import WeightPlateLogo from '../images/store-logo.png';
+import SuperTeamLogo from '../images/cape.png';
+import NoumeLogo from '../images/noume.png';
+import WtwLogo from '../images/film.png';
+
+export const topProjects = [
+    {
+        index: 1,
+        logo: WeightPlateLogo,
+        name: 'Weight Plate',
+        liveLink: 'https://weight-plate.vercel.app/',
+        codeLink: 'https://github.com/wramos1/weight-plate',
+        imgSrc: ['dd'],
+        stack: ['React', 'ShopifyAPI', 'GraphQL', 'NextJS', 'TailwindCSS']
+    },
+    {
+        index: 1,
+        logo: NoumeLogo,
+        name: 'Noume',
+        liveLink: '',
+        codeLink: '',
+        imgSrc: [''],
+        stack: ['React', 'External APIs', 'TailwindCSS', 'HTML']
+    },
+    {
+        index: 3,
+        logo: SuperTeamLogo,
+        name: 'Super Team',
+        liveLink: 'https://willyamramos.dev/super-team',
+        codeLink: 'https://github.com/wramos1/super-team',
+        imgSrc: ['d'],
+        stack: ['React', 'Typescript', 'External APIs', 'CSS', 'HTML']
+    },
+    {
+        index: 4,
+        logo: WtwLogo,
+        name: 'Where To Watch',
+        liveLink: 'https://willyamramos.dev/WhereToWatch/',
+        codeLink: 'https://github.com/wramos1/WhereToWatch',
+        imgSrc: [''],
+        stack: ['React', 'Redux', 'External APIs', 'CSS', 'HTML']
+    }
+]
+
+export const otherProjects = [
+    {
+        name: 'Dad Joke Tier List',
+        description: 'Everyone loves a good dad joke and this website allows you to generate an endless amount of dad jokes and judge them.',
+        stack: ['JavaScript', 'HTML', 'CSS'],
+        liveLink: 'https://willyamramos.dev/DadJokeTierList/',
+        codeLink: 'https://github.com/wramos1/DadJokeTierList',
+    },
+    {
+        name: 'What To Make',
+        description: 'This project fetches recipes for the ingredients you have at home and provides the instructions in an easy-to-read manner. Begin your journey to Master Chef.',
+        stack: ['React', 'Redux', 'External APIs', 'HTML', 'CSS'],
+        liveLink: 'https://willyamramos.dev/what-to-make/',
+        codeLink: 'https://github.com/wramos1/What-To-Make',
+    },
+    {
+        name: 'Fit Scale',
+        description: 'An easy one stop shop where one can find all the information they are bound to look for during their fitness endeavors.',
+        stack: ['React', 'External APIs', 'HTML', 'CSS'],
+        liveLink: 'https://willyamramos.dev/Fit-Scale/',
+        codeLink: 'https://github.com/wramos1/Fit-Scale',
+    }
+]
